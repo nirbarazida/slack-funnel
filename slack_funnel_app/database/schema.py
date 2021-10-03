8a675f3d82a9f4c47af7c2a097bbb1eb9fe363df
Nir Barazida <nir@dagshub.com>
2021-10-03 16:43:33 +0300

    final schema

--- slack_funnel_app/database/schema.py
+++ slack_funnel_app/database/schema.py
@@ -1,12 +1,14 @@
 from slack_funnel_app import db, login_manager
 from datetime import datetime
 from flask_login import UserMixin
+from sqlalchemy.orm import relationship
 
 @login_manager.user_loader
 def load_user(user_id):
     return UserTable.query.get(int(user_id))
 
-class UserTable(db.Model,UserMixin):
+
+class UserTable(db.Model, UserMixin):
     """
     stores all the user that was register to the website.
     relationship:
@@ -19,11 +21,10 @@ class UserTable(db.Model,UserMixin):
     password = db.Column(db.String(60), nullable=False)
     email = db.Column(db.String(30), nullable=False, unique=True)
     image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
-    member_since = db.Column(db.DateTime(),default=datetime.utcnow)
+    member_since = db.Column(db.DateTime(), default=datetime.utcnow)
 
     def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
-
 
     @property
     def is_active(self):
@@ -42,3 +43,196 @@ class UserTable(db.Model,UserMixin):
             return str(self.id)
         except AttributeError:
             raise NotImplementedError('No `id` attribute - override `get_id`')
+
+class Workspace(db.Model):
+    __tablename__ = 'workspaces'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), nullable=False, unique=True)
+    start_date = db.Column(db.DateTime(), default=datetime.utcnow)
+    token = db.Column(db.String(150), nullable=False, unique=True)
+
+    # many 2 many - Workspace -> WorkspaceMessage <- Message
+    message = relationship("WorkspaceMessage", backref='workspace')
+
+    # one 2 many - 1 workflow - workspace N
+    workflow_id = db.Column(db.Integer, db.ForeignKey('workflows.id'))
+    workflow = relationship("Workflow")
+
+
+class Workflow(db.Model):
+    __tablename__ = 'workflows'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), nullable=False, unique=True)
+
+    # many 2 many - Workflow -> WorkflowMessage <- Message
+    message = relationship("WorkflowMessage", backref='workflow')
+
+    # one 2 many - 1 workflow - workspace N
+    workspace = relationship("Workspace")
+
+
+class WorkflowMessage(db.Model):
+    __tablename__ = 'workflow_messages'
+    id = db.Column(db.Integer, primary_key=True)
+    time_from_start_days = db.Column(db.Integer, nullable=False)
+    time_from_start_hours = db.Column(db.Integer, nullable=False)
+
+    # many 2 many - Workflow -> WorkflowMessage <- Message
+    workflow_id = db.Column(db.Integer(), db.ForeignKey("workflows.id"))
+    message_id = db.Column(db.Integer(), db.ForeignKey("messages.id"))
+
+    # one 2 one - WorkflowMessage (ID) - WorkspaceMessage (NonID)
+    workspace_message = relationship('WorkspaceMessage', backref='workflowmessage', uselist=False)
+
+class Message(db.Model):
+    __tablename__ = 'messages'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), nullable=False, unique=True)
+    message = db.Column(db.String(2000), nullable=False, unique=True)
+
+    # many 2 many - Workspace -> WorkspaceMessage <- Message
+    workspace = relationship("WorkspaceMessage", backref='message')
+
+    # many 2 many - Workflow -> WorkflowMessage <- Message
+    workflow = relationship("WorkflowMessage", backref='message')
+
+
+class WorkspaceMessage(db.Model):
+    __tablename__ = 'workspace_messages'
+    id = db.Column(db.Integer, primary_key=True)
+    time_utc = db.Column(db.DateTime(), nullable=False)
+    status = db.Column(db.Boolean, default=0)  # todo check that it works + fill with 0 or 1 not T/F
+
+    # many 2 many - Workspace -> WorkspaceMessage <- Message
+    workspace_id = db.Column(db.Integer(), db.ForeignKey("workspaces.id"))
+    message_id = db.Column(db.Integer(), db.ForeignKey("messages.id"))
+
+    # one 2 one - WorkflowMessage (ID) - WorkspaceMessage (NonID)
+    workflow_message_id = db.Column(db.Integer(), db.ForeignKey('workflow_messages.id'))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class Workspace(db.Model):
+#     __tablename__ = 'workspaces'
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String(30), nullable=False, unique=True)
+#     start_date = db.Column(db.DateTime(), default=datetime.utcnow)
+#     token = db.Column(db.String(150), nullable=False)
+#     workspace_step = relationship("WorkspaceStep") # check
+#
+#     workflow_id = db.Column(db.Integer, db.ForeignKey('workflows.id')) # check
+#     workflow = relationship("Workflow") # check
+#
+#
+#
+# class WorkspaceStep(db.Model):
+#     __tablename__ = 'workspace_steps'
+#     id = db.Column(db.Integer, primary_key=True)
+#
+#     workspace_id = db.Column(db.Integer, db.ForeignKey('workspaces.id')) # check
+#     workspace = relationship("Workspace") # check
+#
+#     step_id = db.Column(db.Integer, db.ForeignKey('steps.id'))  # check
+#     step = relationship("Step")  # check
+#
+#     workflow_step_id = db.Column(db.Integer(), db.ForeignKey('workflows_steps.id')) # check
+#
+#     time_utc = db.Column(db.DateTime(), nullable=False)
+#     status = db.Column(db.Boolean, default=0)  # todo check that it works + fill with 0 or 1 not T/F
+#
+# class WorkflowStep(db.Model):
+#     __tablename__ = 'workflows_steps'
+#     id = db.Column(db.Integer, primary_key=True)
+#
+#     workflow_id = db.Column(db.Integer, db.ForeignKey('workflows.id')) # check
+#     workflow = relationship("Workflow") # check
+#
+#     step_id = db.Column(db.String(30), db.ForeignKey('steps.name'))  # check
+#     step = relationship("Step") # check
+#
+#     time_from_start_days = db.Column(db.Integer, nullable=False)
+#     time_from_start_hours = db.Column(db.Integer, nullable=False)
+#
+#     workspace_step = relationship('WorkspaceStep', backref='workspacestep', uselist=False) # check
+#
+# class Workflow(db.Model):
+#     __tablename__ = 'workflows'
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String(30), nullable=False, unique=True)
+#     workspace = relationship("Workspace") # check
+#     workflow_step = relationship("WorkflowStep") # check
+#
+#
+# class Step(db.Model):
+#     __tablename__ = 'steps'
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String(30), nullable=False, unique=True)
+#     message = db.Column(db.String(2000), nullable=False, unique=True)
+#     workflow_step = relationship("WorkflowStep") # check
+#     workspace_step = relationship("WorkspaceStep") #check
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
