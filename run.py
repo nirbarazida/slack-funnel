83de8253d092a854d45644915dc0d9cbb085c9ab
Nir Barazida <nir@dagshub.com>
2021-10-03 10:52:32 +0300

    database, User Authentication

--- run.py
+++ run.py
@@ -1,9 +1,47 @@
-from flask import Flask, render_template, url_for, flash, redirect
-from routes_and_forms.forms import RegistrationForm, LoginForm
+from datetime import datetime
+from flask import render_template, url_for, flash, redirect
+from slack_funnel_app.routes_and_forms.forms import RegistrationForm, LoginForm
 
-app = Flask(__name__)
 
-app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+
+class UserTable(db.Model):
+    """
+    stores all the user that was register to the website.
+    relationship:
+        Email - one to many
+    """
+
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), nullable=False, unique=True)
+    password = db.Column(db.String(60), nullable=False)
+    email = db.Column(db.String(30), nullable=False, unique=True)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    member_since = db.Column(db.DateTime(),default=datetime.utcnow)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+
+    @property
+    def is_active(self):
+        return True
+
+    @property
+    def is_authenticated(self):
+        return True
+
+    @property
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        try:
+            return str(self.id)
+        except AttributeError:
+            raise NotImplementedError('No `id` attribute - override `get_id`')
+
+
 
 posts = [
     {
